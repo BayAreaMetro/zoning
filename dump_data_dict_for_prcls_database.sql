eff836850dc6ec6f22d569ab953e9193537a6f3b
Tom Buckley <buckley.tom@example.com>
2015-02-11 17:08:32 -0800

    setting up postgres db from legacy data

---
+++ dump_data_dict_for_prcls_database.sql
@@ -0,0 +1,4 @@
+﻿Copy (select *, data_type from information_schema.columns
+WHERE table_name = 'parcels_mpg'
+ORDER BY column_name) 
+To '/legacy/prcls_data_dict.csv' With CSV;
---
+++ populate_legacy_zoning_table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE lgcy_prcls_znng
+(prcl_id int, znng_id float, juris varchar);
+
+COPY lgcy_prcls_znng FROM '\legacy\zoning\prcls_znng.csv' DELIMITER ',' CSV HEADER;
+
+ALTER TABLE lgcy_prcls_znng ALTER COLUMN znng_id TYPE int USING znng_id::int;
