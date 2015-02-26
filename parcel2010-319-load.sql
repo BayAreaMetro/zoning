98d60c69298cbb7d039fed8e08b6c0137eae7898
Tom Buckley <buckley.tom@example.com>
2015-02-25 16:38:31 -0800

    qgis can't see views; add spatial index

--- parcel2010-319-load.sql
+++ parcel2010-319-load.sql
@@ -6,7 +6,9 @@ parcel_id integer
 
 COPY tmp_sample319 FROM '/zoning_data/geography_zoning_parcel_relation_3_19.txt' WITH (FORMAT csv, DELIMITER E'\t', HEADER TRUE);
 
-CREATE VIEW zoning.parcels319_geo AS
+CREATE TABLE zoning.parcels319_geo AS
 SELECT p2.joinnuma, p1.parcel_id, p1.zoning, p2.geom,
 FROM zoning.parcels319 as p1
     INNER JOIN public.parcels_mpg as p2 ON p1.parcel_id = p2.joinnuma;
+
+CREATE INDEX parcels319_geo ON zoning.parcels319_geo USING GIST (geom);
