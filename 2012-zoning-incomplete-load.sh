5fa941c00eee4d5aba447c7ca628e628b222af56
unknown <tbuckl@LUMODEL.mtc.local>
2015-02-27 08:46:14 -0800

    remove unused import files

--- 2012-zoning-incomplete-load.sh
+++
@@ -1,3 +0,0 @@
-DBUSERNAME=$1
-DBPASSWORD=$2
-ogr2ogr -f "PostgreSQL" PG:"host=localhost user=DBUSERNAME dbname=lgcy_znng_2012 password=DBPASSWORD port=5432" /d/legacy/zoning/zoning_2012.gdb -a_srs EPSG:26910
