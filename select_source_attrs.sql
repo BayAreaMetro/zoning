0a975619c4d9a197a4be47ba1c68aaa1acb6d3b0
Tom Buckley <buckley.tom@gmail.com>
2015-03-05 13:29:26 -0800

    use if else instead of case

--- select_source_attrs.sql
+++ select_source_attrs.sql
@@ -1,35 +1,35 @@
-CREATE OR REPLACE FUNCTION select_source_attr(pgeom geometry)
+CREATE OR REPLACE FUNCTION select_source_att(pgeom geometry)
   RETURNS text AS
 $BODY$
 DECLARE 
 result text;
 sql_string text := '';
-BEGIN	
-select case
-         when exists (
- 			SELECT * FROM
+BEGIN
+	IF EXISTS(
+			SELECT * FROM
 			zoning_legacy_2012.lookup as z
 			WHERE ST_Intersects(z.geom, pgeom)
 			LIMIT 1
 			)
-         then 
-			sql_string := (SELECT 'SELECT hstore(g.*) 
-					FROM (SELECT (r).* 
-					FROM (SELECT (t #= hstore(''wkb_geometry'',null)) as r 
-					FROM zoning_legacy_2012.' || 
-					quote_ident(z.tablename) || 
-					' as t WHERE ogc_fid=' || 
-					z.ogc_fid || ') s) g '
-			FROM
-			zoning_legacy_2012.lookup as z
-			WHERE ST_Intersects(z.geom, pgeom)
-			LIMIT 1);
-         else 
-         	sql_string := '';
-       end
+	THEN
+		sql_string := (SELECT 'SELECT hstore(g.*) 
+				FROM (SELECT (r).* 
+				FROM (SELECT (t #= hstore(''wkb_geometry'',null)) as r 
+				FROM zoning_legacy_2012.' || 
+				quote_ident(z.tablename) || 
+				' as t WHERE ogc_fid=' || 
+				z.ogc_fid || ') s) g '
+		FROM
+		zoning_legacy_2012.lookup as z
+		WHERE ST_Intersects(z.geom, pgeom)
+		LIMIT 1);
 	RAISE NOTICE '(%)', sql_string;
 	EXECUTE sql_string INTO result;
-	RETURN result;
+	ELSE
+	RAISE NOTICE 'no intersection';
+	result := 'no intersecting source';	
+	END IF;
+RETURN result;
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
