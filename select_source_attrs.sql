c993cd2bda058ae74a92b457e6e2a37272ced76a
Tom Buckley <buckley.tom@gmail.com>
2015-03-05 13:25:37 -0800

    error handling-parcels w/o intersecting zoning

--- select_source_attrs.sql
+++ select_source_attrs.sql
@@ -5,18 +5,28 @@ DECLARE
 result text;
 sql_string text := '';
 BEGIN	
-	sql_string := (SELECT 'SELECT hstore(g.*) 
-			FROM (SELECT (r).* 
-			FROM (SELECT (t #= hstore(''wkb_geometry'',null)) as r 
-			FROM zoning_legacy_2012.' || 
-			quote_ident(z.tablename) || 
-			' as t WHERE ogc_fid=' || 
-			z.ogc_fid || ') s) g '
-	FROM
-	zoning_legacy_2012.lookup as z
---	zoning.parcels_generic_types as p
-	WHERE ST_Intersects(z.geom, pgeom)
-	LIMIT 1);
+select case
+         when exists (
+ 			SELECT * FROM
+			zoning_legacy_2012.lookup as z
+			WHERE ST_Intersects(z.geom, pgeom)
+			LIMIT 1
+			)
+         then 
+			sql_string := (SELECT 'SELECT hstore(g.*) 
+					FROM (SELECT (r).* 
+					FROM (SELECT (t #= hstore(''wkb_geometry'',null)) as r 
+					FROM zoning_legacy_2012.' || 
+					quote_ident(z.tablename) || 
+					' as t WHERE ogc_fid=' || 
+					z.ogc_fid || ') s) g '
+			FROM
+			zoning_legacy_2012.lookup as z
+			WHERE ST_Intersects(z.geom, pgeom)
+			LIMIT 1);
+         else 
+         	sql_string := '';
+       end
 	RAISE NOTICE '(%)', sql_string;
 	EXECUTE sql_string INTO result;
 	RETURN result;
