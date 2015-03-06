1d3d0706cc1049d2bc029f5919a7417061745993
Tom Buckley <buckley.tom@gmail.com>
2015-03-06 15:11:40 -0800

    return hstore instead of string

--- select_zoning_qa_table.sql
+++ select_zoning_qa_table.sql
@@ -1,4 +1,4 @@
-SELECT  joinnuma, zoning.select_generic_source(joinnuma), select_source_att(j.geom) FROM (SELECT joinnuma, geom
+SELECT joinnuma, zoning.select_generic_source(joinnuma), select_source_att(j.geom)::hstore FROM (SELECT joinnuma, geom
 FROM zoning.auth_geo
 ORDER BY random()
-LIMIT 400) j
+LIMIT 20) j
