c63a5b2c9c549ebda41ab362123e95c0246c5c48
Tom Buckley <buckley.tom@gmail.com>
2015-03-06 15:56:09 -0800

    Fixup Load Script & QA Stub
    skip rows with invalid geometries in loading script for source zoning GDB
    stub for python to organize results from zoning query

--- load-2012-zoning.sh
+++ load-2012-zoning.sh
@@ -1,28 +1,44 @@
 DBUSERNAME=$1
 DBPASSWORD=$2
 DBHOST=$3
-DBNAME=$4
+DBPORT=$4
+DBNAME=$5
 
-ogr2ogr -f "PostgreSQL" \
-PG:"host=${DBHOST} port=5432 dbname=${DBNAME} user=${DBUSERNAME} password=${DBPASSWORD}" \
+# RUN THESE COMMENTED COMMANDS IN THE VM, OR ELSE DO THEM IN PGADMIN
+# #drop if db exists
+# sudo -u postgres psql legacy_2012_zoning -c "create extension postgis;"
+# sudo -u postgres psql legacy_2012_zoning -c "create extension postgis_topology;"
+
+ogr2ogr -skipfailures -f "PostgreSQL" \
+PG:"host=${DBHOST} port=${DBPORT} dbname=${DBNAME} user=${DBUSERNAME} password=${DBPASSWORD}" \
 data/PlannedLandUsePhase1.gdb
 
-ogr2ogr -f "PostgreSQL" \
-PG:"host=${DBHOST} port=5432 dbname=${DBNAME} user=${DBUSERNAME} password=${DBPASSWORD}" \
+ogr2ogr -skipfailures -f "PostgreSQL" \
+PG:"host=${DBHOST} port=${DBPORT} dbname=${DBNAME} user=${DBUSERNAME} password=${DBPASSWORD}" \
 data/PlannedLandUsePhase2.gdb
 
-ogr2ogr -f "PostgreSQL" \
-PG:"host=${DBHOST} port=5432 dbname=${DBNAME} user=${DBUSERNAME} password=${DBPASSWORD}" \
+ogr2ogr -skipfailures -f "PostgreSQL" \
+PG:"host=${DBHOST} port=${DBPORT} dbname=${DBNAME} user=${DBUSERNAME} password=${DBPASSWORD}" \
 data/PlannedLandUsePhase3.gdb
 
-ogr2ogr -f "PostgreSQL" \
-PG:"host=${DBHOST} port=5432 dbname=${DBNAME} user=${DBUSERNAME} password=${DBPASSWORD}" \
+ogr2ogr -skipfailures -f "PostgreSQL" \
+PG:"host=${DBHOST} port=${DBPORT} dbname=${DBNAME} user=${DBUSERNAME} password=${DBPASSWORD}" \
 data/PlannedLandUsePhase4.gdb
 
-ogr2ogr -f "PostgreSQL" \
-PG:"host=${DBHOST} port=5432 dbname=${DBNAME} user=${DBUSERNAME} password=${DBPASSWORD}" \
+ogr2ogr -skipfailures -f "PostgreSQL" \
+PG:"host=${DBHOST} port=${DBPORT} dbname=${DBNAME} user=${DBUSERNAME} password=${DBPASSWORD}" \
 data/PlannedLandUsePhase5.gdb
 
-ogr2ogr -f "PostgreSQL" \
-PG:"host=${DBHOST} port=5432 dbname=${DBNAME} user=${DBUSERNAME} password=${DBPASSWORD}" \
+ogr2ogr -skipfailures -f "PostgreSQL" \
+PG:"host=${DBHOST} port=${DBPORT} dbname=${DBNAME} user=${DBUSERNAME} password=${DBPASSWORD}" \
 data/PlannedLandUsePhase6.gdb
+
+# sudo -u postgres legacy_2012_zoning -c 'ALTER SCHEMA public RENAME TO zoning_legacy_2012;'
+# sudo -u postgres legacy_2012_zoning -c 'ALTER TABLE zoning_legacy_2012.export_output RENAME TO zoning_legacy_2012.monte_sereno;'
+
+# sudo -u postgres legacy_2012_zoning -skipfailures -f scraps/alter_table_multiple_schema.sql
+
+# rm legacy2012.dump
+# sudo -u postgres pg_dump legacy_2012_zoning > legacy2012.dump
+# sudo -u postgres psql landuse < legacy2012.dump
+
