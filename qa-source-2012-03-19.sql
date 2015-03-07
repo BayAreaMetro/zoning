0588cfc9d61d497de47ae6251b33248813429c2b
Tom Buckley <buckley.tom@gmail.com>
2015-03-06 18:09:25 -0800

    remove sourcing and qa files
    since the 03-19-2012 csv seems to have more parcels than 3-29-2012 we are
    going with that one. these removed files are no longer necessary to source
    and qa in master

--- qa-source-2012-03-19.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW zoning.parcels_notin03_19_2012 AS
-SELECT p1.parcel_id, p2.joinnuma
-FROM zoning.parcels03_19_2012 as p1
-    RIGHT JOIN public.parcels_mpg as p2 ON p1.parcel_id = p2.joinnuma;
-SELECT count(*) FROM zoning.parcels_notin03_19_2012 WHERE parcel_id IS NULL;
---~68844
