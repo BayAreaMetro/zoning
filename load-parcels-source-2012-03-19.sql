4cf4afd6ad0ab53b250abbab183608bc07c6c515
Tom Buckley <buckley.tom@gmail.com>
2015-03-06 18:33:45 -0800

    using joinnuma for parcel_id

--- load-parcels-source-2012-03-19.sql
+++ load-parcels-source-2012-03-19.sql
@@ -3,7 +3,7 @@
 CREATE TABLE zoning.parcels03_19_12 (
 id integer,
 zoning integer,
-parcel_id integer
+joinnuma integer
 );
 
 COPY zoning.parcels03_19_12 FROM '/zoning_data/geography_zoning_parcel_relation_3_19.txt' WITH (FORMAT csv, DELIMITER E'\t', HEADER TRUE);
@@ -11,8 +11,8 @@ COPY zoning.parcels03_19_12 FROM '/zoning_data/geography_zoning_parcel_relation_
 --create authoritative parcel data table
 
 CREATE TABLE zoning.parcels_auth 
-AS SELECT a.parcel_id,a.zoning_id 
-FROM (SELECT parcel_id, zoning 
+AS SELECT a.joinnuma,a.zoning_id 
+FROM (SELECT joinnuma, zoning 
 	as zoning_id 
 	FROM zoning.parcels03_19_2012) 
 	as a;
@@ -26,8 +26,8 @@ COPY tmp_x FROM '/zoning_data/csv_process/ParcelUpdateMay21BerkeleyDowntownZonin
 ALTER TABLE tmp_x ALTER COLUMN joinnuma TYPE int USING joinnuma::int;
 ALTER TABLE tmp_x ALTER COLUMN zoneid TYPE int USING zoneid::int;
 
-INSERT INTO zoning.parcels_auth (parcel_id,zoning_id)
-SELECT tmp_x.joinnuma as parcel_id, tmp_x.zoningid as zoning_id
+INSERT INTO zoning.parcels_auth (joinnuma,zoning_id)
+SELECT tmp_x.joinnuma as joinnuma, tmp_x.zoningid as zoning_id
 FROM tmp_x;
 
 DROP TEMP TABLE tmp_x;
@@ -39,15 +39,19 @@ COPY tmp_x FROM '/zoning_data/csv_process/ParcelUpdateMay21RichmondZoning.csv' W
 ALTER TABLE tmp_x ALTER COLUMN joinnuma TYPE int USING joinnuma::int;
 ALTER TABLE tmp_x ALTER COLUMN zoneid TYPE int USING zoneid::int;
 
-INSERT INTO zoning.parcels_auth (parcel_id,zoning_id)
-SELECT tmp_x.joinnuma as parcel_id, tmp_x.zoningid as zoning_id
+INSERT INTO zoning.parcels_auth (joinnuma,zoning_id)
+SELECT tmp_x.joinnuma as joinnuma, tmp_x.zoningid as zoning_id
 FROM tmp_x;
 
+CREATE TABLE zoning.auth_geo AS
+SELECT p2.joinnuma, p1.zoning_id, p2.geom
+FROM zoning.parcels_auth as p1
+    RIGHT JOIN public.parcels_mpg as p2 ON p1.joinnuma = p2.joinnuma;
 
 /*THIS SHOULDN'T BE NECESSARY NOW
 CREATE TABLE zoning.parcels03_19_geo AS
-SELECT p2.joinnuma, p1.parcel_id, p1.zoning, p2.geom
+SELECT p2.joinnuma, p1.joinnuma, p1.zoning, p2.geom
 FROM zoning.parcels319 as p1
-    INNER JOIN public.parcels_mpg as p2 ON p1.parcel_id = p2.joinnuma;
+    INNER JOIN public.parcels_mpg as p2 ON p1.joinnuma = p2.joinnuma;
 
 CREATE INDEX parcels319_geo_idx ON zoning.parcels319_geo USING GIST (geom);*/
