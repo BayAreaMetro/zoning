2771e5aa21d7e6ba1a8740cbd1d76fbc3d7531a7
Tom Buckley <buckley.tom@gmail.com>
2015-03-10 11:19:14 -0700

    change table names to parcels/zoning in theory

--- scraps/polygon_area_within_other_polygon.sql
+++ scraps/polygon_area_within_other_polygon.sql
@@ -5,9 +5,9 @@ SELECT id,
        sum(ST_Area(geom)) area, 
        round(sum(ST_Area(geom))/min(bufferarea) * 1000) / 10 prop, 
        ST_Union(geom) geom
-FROM (SELECT buf.id, ctype, ST_Area(buf.geom) bufferarea, ST_Intersection(buf.geom, c.geom) geom
-      FROM d_random_buffers_fm_1000_mtm7 buf, a_forestcover_mtm7 c
-      WHERE ST_Intersects(buf.geom, c.geom)) foo
+FROM (SELECT p.id, ctype, ST_Area(p.geom) bufferarea, ST_Intersection(p.geom, z.geom) geom --does p.id need to be unique or should be joinnuma?
+      FROM parcels buf, source_zoning z
+      WHERE ST_Intersects(p.geom, z.geom)) foo
 GROUP BY id, ctype
 ORDER BY id, area DESC, ctype;
 
