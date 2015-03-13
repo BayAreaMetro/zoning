de87b893123be73f8e9ad71a830aed3b05959217
Tom Buckley <buckley.tom@gmail.com>
2015-03-13 07:37:42 -0700

    stubs for dealing with multiple source zoning intersections--counting land area

--- scraps/polygon_area_within_other_polygon.sql
+++ scraps/polygon_area_within_other_polygon.sql
@@ -1,12 +1,11 @@
 --kindly shared by Pierre Racine from the Centre d'étude de la forêt at FOSS4G-NA 2015
-CREATE TABLE d_random_buffers_fm_coverarea_1000_mtm7 AS
-SELECT id, 
+SELECT parcel_id, 
        ctype, 
        sum(ST_Area(geom)) area, 
        round(sum(ST_Area(geom))/min(bufferarea) * 1000) / 10 prop, 
        ST_Union(geom) geom
-FROM (SELECT p.id, ctype, ST_Area(p.geom) bufferarea, ST_Intersection(p.geom, z.geom) geom --does p.id need to be unique or should be joinnuma?
-      FROM parcels buf, source_zoning z
+FROM (SELECT p.parcel_id, z.zoning_id, ST_Area(p.geom) parcelarea, ST_Intersection(p.geom, z.geom) geom --does p.id need to be unique or should be joinnuma?
+      FROM parcels p, source_zoning z
       WHERE ST_Intersects(p.geom, z.geom)) foo
 GROUP BY id, ctype
 ORDER BY id, area DESC, ctype;
