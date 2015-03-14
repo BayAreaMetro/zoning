886da4d73cf93279162bdb1caf6bb64e74081e29
Tom Buckley <buckley.tom@gmail.com>
2015-03-13 20:05:35 -0700

    fix for invalid geoms, also using spandex parcels

--- proportions_of_parcels_with_multiple_overlapping_zones.sql
+++ proportions_of_parcels_with_multiple_overlapping_zones.sql
@@ -1,16 +1,22 @@
+CREATE TABLE zoning.lookup_2012_valid AS
+SELECT 
+	ogc_fid, tablename, ST_MakeValid(geom)
+FROM
+	zoning_legacy_2012.lookup;
+
 CREATE TABLE zoning.parcels_source_2012_overlaps AS
 SELECT 
-	joinnuma,
+	parcel_id,
 	ogc_fid,
 	sum(ST_Area(geom)) area,
 	round(sum(ST_Area(geom))/min(parcelarea) * 1000) / 10 prop,
 	ST_Union(geom) geom
-FROM (SELECT p.joinnuma, z.ogc_fid, ST_Area(p.geom) parcelarea, ST_Intersection(p.geom, z.geom) geom --does p.id need to be unique or should be joinnuma?
+FROM (SELECT p.parcel_id, z.ogc_fid, ST_Area(p.geom) parcelarea, ST_Intersection(p.geom, z.st_makevalid) geom --does p.id need to be unique or should be joinnuma?
 		FROM
-		zoning_legacy_2012.lookup z, 
-		zoning.auth_geo p
+		zoning.lookup_2012_valid z, 
+		parcel p
       	WHERE (SELECT count(*) FROM
-			zoning_legacy_2012.lookup as z, zoning.auth_geo p
-			WHERE ST_Intersects(z.geom, p.geom))>1
+			zoning.lookup_2012_valid as z, parcel p
+			WHERE ST_Intersects(z.st_makevalid, p.geom))>1
       	) foo
-GROUP BY joinnuma, ogc_fid;
+GROUP BY parcel_id, ogc_fid;
