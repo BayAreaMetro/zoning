38cd2b651752ea41819bf9740be5aeb8a82516f6
Tom Buckley <buckley.tom@gmail.com>
2015-03-15 16:33:26 -0700

    fixed geometries and query
    project spandex parcels in same espg as all other data
    using st_makevalid and removing geometry collections and empty geoms
    tried saving parcel intersection query to table b/c takes long
    also improved query to actually select columns

--- proportions_of_parcels_with_multiple_overlapping_zones.sql
+++ proportions_of_parcels_with_multiple_overlapping_zones.sql
@@ -1,22 +1,67 @@
+ALTER TABLE parcel 
+   ALTER COLUMN geom 
+   TYPE Geometry(MultiPolygon, 26910) 
+   USING ST_Transform(geom, 26910);
+
 CREATE TABLE zoning.lookup_2012_valid AS
 SELECT 
-	ogc_fid, tablename, ST_MakeValid(geom)
+	ogc_fid, tablename, ST_MakeValid(geom) geom
 FROM
 	zoning_legacy_2012.lookup;
 
-CREATE TABLE zoning.parcels_source_2012_overlaps AS
+CREATE INDEX lookup_2012_valid_gidx ON zoning.lookup_2012_valid USING GIST (geom);
+
+CREATE TABLE zoning.lookup_2012_problem_geoms as
+SELECT *
+FROM zoning.lookup_2012_valid
+WHERE st_isempty(st_centroid(geom));
+
+INSERT INTO zoning.lookup_2012_problem_geoms (
+SELECT *
+FROM zoning.lookup_2012_valid
+WHERE GeometryType(geom) = 'GEOMETRYCOLLECTION')
+
+DELETE FROM zoning.lookup_2012_valid
+WHERE GeometryType(geom) = 'GEOMETRYCOLLECTION';
+
+DELETE FROM zoning.lookup_2012_valid
+WHERE st_isempty(st_centroid(geom));
+
+CREATE TABLE zoning.parcels_with_multiple_zoning AS
+SELECT * from parcel where parcel_id
+IN (SELECT parcel_id FROM (SELECT parcel_id, count(*) as countof FROM
+			zoning.lookup_2012_valid as z, parcel p
+			WHERE ST_Intersects(z.geom, p.geom)
+			GROUP BY parcel_id
+			) a WHERE countof>1)
+
+-- COULD ALSO CACHE THE FOLLOWING, OR COMBINE WITH ABOVE 
+-- e.g. (select *, intersection from parcel,zoning)
+--
+-- CREATE TABLE pz AS
+-- SELECT *, ST_Intersection(z.geom,p.geom) FROM
+-- 		zoning.lookup_2012_valid as z, parcel p
+-- 		WHERE ST_Intersects(z.geom, p.geom)
+
+CREATE TABLE zoning.parcel_overlaps AS
 SELECT 
 	parcel_id,
 	ogc_fid,
+	tablename,
 	sum(ST_Area(geom)) area,
 	round(sum(ST_Area(geom))/min(parcelarea) * 1000) / 10 prop,
 	ST_Union(geom) geom
-FROM (SELECT p.parcel_id, z.ogc_fid, ST_Area(p.geom) parcelarea, ST_Intersection(p.geom, z.st_makevalid) geom --does p.id need to be unique or should be joinnuma?
-		FROM
-		zoning.lookup_2012_valid z, 
-		parcel p
-      	WHERE (SELECT count(*) FROM
-			zoning.lookup_2012_valid as z, parcel p
-			WHERE ST_Intersects(z.st_makevalid, p.geom))>1
-      	) foo
-GROUP BY parcel_id, ogc_fid;
+FROM (
+SELECT p.parcel_id, 
+	z.ogc_fid, 
+	z.tablename,
+ 	ST_Area(p.geom) parcelarea, 
+ 	ST_Intersection(p.geom, z.geom) geom
+FROM zoning.parcels_with_multiple_zoning p,
+zoning.lookup_2012_valid as z
+WHERE ST_Intersects(z.geom, p.geom) f
+) f
+GROUP BY 
+	parcel_id,
+	ogc_fid,
+	tablename;
