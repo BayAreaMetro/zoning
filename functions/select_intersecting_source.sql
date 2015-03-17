9ccb6821c5939af27ef27a90815887e8d0e9f817
Tom Buckley <buckley.tom@gmail.com>
2015-03-17 16:22:00 -0700

    select field from source table-plpgsql
    this function selects the value in the source field of the appropriate source table  based on another select

--- functions/select_intersecting_source.sql
+++ functions/select_intersecting_source.sql
@@ -1,6 +1,30 @@
-select a.parcel_id, a.geom, a.tablename, s.matchfield
-FROM 
-zoning.source_field_name s,
-(select *
-from pz limit 1) a
-WHERE s.tablename = a.tablename;
+CREATE OR REPLACE FUNCTION test_zone()
+  RETURNS text AS
+$BODY$
+DECLARE 
+result text;
+sql_string text := '';
+BEGIN
+	sql_string := (SELECT 'SELECT '
+			|| qry.matchfield ||    
+			' FROM (SELECT (r).* 
+			FROM (SELECT (t #= hstore(''wkb_geometry'',null)) as r 
+			FROM zoning_legacy_2012.' || 
+			quote_ident(qry.tablename) || 
+			' as t WHERE ogc_fid=' || 
+			qry.ogc_fid || ') s) g '
+		FROM
+			(select a.parcel_id, a.geom, a.tablename, a.ogc_fid, s.matchfield
+			FROM 
+			zoning.source_field_name s,
+				(select *
+				from pz limit 1) a
+				WHERE s.tablename = a.tablename) qry
+	--LIMIT 1
+	);
+	RAISE NOTICE '(%)', sql_string;
+	EXECUTE sql_string INTO result;
+RETURN result;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
