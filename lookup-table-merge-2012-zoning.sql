d15f8746eab3ab4070eef5ca54e89890ac049c24
Tom Buckley <buckley.tom@example.com>
2015-03-20 10:40:42 -0700

    do lookup table merge with matchfields in order to join to generic zoning table

--- lookup-table-merge-2012-zoning.sql
+++ lookup-table-merge-2012-zoning.sql
@@ -1,15 +1,14 @@
-CREATE TABLE zoning_legacy_2012.lookup (
-	ogc_fid integer,
-	tablename text, 
-	geom geometry 
-);
+-- Function: public.mergetables(text)
 
---the structure from this is from some nice person on stackoverflow, but i closed that tab and can't find it right now
-CREATE OR REPLACE FUNCTION mergetables(sql_string text) RETURNS TEXT AS $$
+-- DROP FUNCTION public.mergetables(text);
+
+CREATE OR REPLACE FUNCTION public.mergetables(sql_string text)
+  RETURNS text AS
+$BODY$
 DECLARE
     	tables CURSOR FOR SELECT *
 	      FROM information_schema.tables
-	      WHERE table_schema = 'public'
+	      WHERE table_schema = 'zoning_legacy_2012'
 	      ORDER BY "table_name" ASC
 	      LIMIT ((SELECT count(*)
 		  FROM information_schema.tables
@@ -17,20 +16,28 @@ DECLARE
 		sql_string text := '';
 BEGIN
 	 FOR table_record IN tables LOOP
-	 sql_string := sql_string || '
-		 insert into lookup
-		 select ogc_fid, ' || quote_LITERAL(table_record."table_name") || ', ST_Force2D(wkb_geometry) from ' || quote_IDENT(table_record."table_name") || ';';
-	 END LOOP;
-	 RAISE NOTICE 'Table_record:(%)', sql_string;
-	 RETURN sql_string;
+	 sql_string := (SELECT '
+		 insert into zoning.lookup_new
+		 select ogc_fid, ' 
+		 || quote_LITERAL(table_record."table_name") || 
+		 ', ST_Force2D(wkb_geometry) ' 
+		 || ', CAST('
+		 || qry.matchfield ||    
+		 ' as text) as zoning, ' 
+		 || qry.juris ||
+		 ' from zoning_legacy_2012.'
+		 || table_record."table_name" 
+		FROM
+			(
+				select matchfield, juris
+					FROM zoning.source_field_name s
+				WHERE s.tablename = table_record."table_name") qry
+			);
+	 RAISE NOTICE '(%)', sql_string;
 	 EXECUTE sql_string;
+	 END LOOP;
+	 RETURN sql_string;
+	 --EXECUTE sql_string;
 END;
-$$ LANGUAGE plpgsql;
-SELECT mergetables('');
-
-ALTER TABLE zoning_legacy_2012.lookup ADD COLUMN id INTEGER;
-CREATE SEQUENCE zoning_legacy_2012_lookup_seq;
-UPDATE zoning_legacy_2012.lookup  SET id = nextval('zoning_legacy_2012_lookup_seq');
-ALTER TABLE zoning_legacy_2012.lookup ALTER COLUMN id SET DEFAULT nextval('zoning_legacy_2012_lookup_seq');
-ALTER TABLE zoning_legacy_2012.lookup ALTER COLUMN id SET NOT NULL;
-ALTER TABLE zoning_legacy_2012.lookup ADD PRIMARY KEY (id);
+$BODY$
+  LANGUAGE plpgsql VOLATILE
