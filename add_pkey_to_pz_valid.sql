2a720f3f6c653a89afd92d601f062e033363a759
Tom Buckley <buckley.tom@example.com>
2015-03-24 16:13:42 -0700

    cleanup
    these lines do not correspond to the file name and they already exist in fix_errors_in_match_field

--- add_pkey_to_pz_valid.sql
+++ add_pkey_to_pz_valid.sql
@@ -4,23 +4,3 @@ UPDATE pz_valid  SET id = nextval('pz_valid_id_seq');
 ALTER TABLE pz_valid ALTER COLUMN id SET DEFAULT nextval('pz_valid_id_seq');
 ALTER TABLE pz_valid ALTER COLUMN id SET NOT NULL;
 ALTER TABLE pz_valid ADD PRIMARY KEY (id);
-
-UPDATE zoning.source_field_name
-SET matchfield = 'zoning'
-WHERE juris=27
-
-UPDATE zoning.source_field_name 
-SET tablename = 'alamedacountygp2006db'
-where tablename like '%alamedagp2%'
-
-UPDATE zoning.source_field_name 
-SET matchfield = 'new_zoning'
-where tablename = 'elcerritozoning'
-
-UPDATE zoning.source_field_name 
-SET matchfield = 'urbsimlu'
-where tablename = 'fremontgeneralplan'
-
-UPDATE zoning.source_field_name 
-SET matchfield = 'dxf_text'
-where tablename = 'orinda_zoning'
