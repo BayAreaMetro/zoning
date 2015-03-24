9282d0598dd2a968e5c6eda1695572fcf11d697f
Tom Buckley <buckley.tom@example.com>
2015-03-24 16:26:18 -0700

    cleanup-removing stale work
    much of these are related to the previous iteration of the zoning/parcel table, which was oriented towards quickly checking the quality of the best candidate zoning/parcel CSV that already existed. removing it since we have now moved on to a process which it is not central to

--- add_pkey_to_pz_valid.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE pz_valid ADD COLUMN id INTEGER;
-CREATE SEQUENCE pz_valid_id_seq;
-UPDATE pz_valid  SET id = nextval('pz_valid_id_seq');
-ALTER TABLE pz_valid ALTER COLUMN id SET DEFAULT nextval('pz_valid_id_seq');
-ALTER TABLE pz_valid ALTER COLUMN id SET NOT NULL;
-ALTER TABLE pz_valid ADD PRIMARY KEY (id);
