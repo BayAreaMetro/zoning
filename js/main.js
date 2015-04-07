39301c3c38b3736c9b5a179fb24a7e332bfefcaf
Tom Buckley <buckley.tom@gmail.com>
2015-04-07 13:39:32 -0700

    use stamen toner background

--- js/main.js
+++ js/main.js
@@ -6,10 +6,14 @@ function init(){
 
 var map = L.map('map').setView([37.7, -122.4], 11);
 
+// replace "toner" here with "terrain" or "watercolor"
+var layer = new L.StamenTileLayer("toner");
+map.addLayer(layer);
+
 var zoning_wms = L.tileLayer.wms("http://10.1.1.204:8081/geoserver/gwc/service/wms?", {
     layers: 'mtc:all_codes_colors',
     styles: 'mtc:all_colors',
-    format: 'image/jpeg',
+    format: 'image/png',
     transparent: true,
     version: '1.1.0',
     attribution: "MTC"
