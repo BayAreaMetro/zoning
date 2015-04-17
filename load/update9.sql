c4da723ae7542dab160b88d761d3920f205d69c3
Tom Buckley <buckley.tom@gmail.com>
2015-04-17 14:56:35 -0700

    update makefile
    give variable names to s3 call and psql args
    organize targets by action taken (get, load, process)
    fix update9 loading

--- load/update9.sql
+++ load/update9.sql
@@ -2,7 +2,7 @@
 CREATE TABLE zoning.update9
 (JOINNUMA numeric,ID numeric,JURIS numeric,CITY numeric,NAME text,MIN_FAR text,MAX_FAR text,MAX_HEIGHT text,MIN_FRONT_ text,MAX_FRONT_ text,SIDE_SETBA text,REAR_SETBA text,MIN_DUA text,MAX_DUA text,COVERAGE text,MAX_DU_PER text,MIN_LOT_SI text,BUILDING_T text,HS text,HT text,HM text,OF_ text,HO text,SC text,IL text,IW text,IH text,RS text,RB text,MR text,MT text, ME text);
 
-COPY zoning.update9 FROM '/zoning_data/Parcels2010_Update9.csv' DELIMITER ',' CSV HEADER;
+COPY zoning.update9 FROM 'source_data/Parcels2010_Update9.csv' DELIMITER ',' CSV HEADER;
 
 ALTER TABLE zoning.update9 ALTER COLUMN id TYPE int USING id::int;
 ALTER TABLE zoning.update9 ALTER COLUMN joinnuma TYPE int USING joinnuma::int;
