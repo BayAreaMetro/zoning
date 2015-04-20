549d6fa34092c397af67666f6a4bb14bee5b230c
tom buckley <buckley.tom@gmail.com>
2015-04-20 23:33:33 +0000

    clean out unused files

--- scraps/intersect_parcels_places.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE zoning.parcels_in_places AS
-SELECT n.name, n.name_1, p.joinnuma, p.geom from parcels_mpg_min as p 
-	LEFT JOIN administrative.places as n ON ST_Intersects(n.geom, p.geom);
-
----FASTER:
-CREATE TABLE zoning.parcels_not_in_places AS
-SELECT n.name, n.name_1, p.joinnuma, p.geom 
-FROM parcels_mpg_min as p, administrative.places as n 
-WHERE NOT ST_Intersects(n.geom, p.geom);
--- scraps/select_heterogeneous-rows-from-source.sql
+++
@@ -1,4 +0,0 @@
-SELECT joinnuma, zoning.select_generic_source(joinnuma), select_source_att(j.geom)::hstore FROM (SELECT joinnuma, geom
-FROM zoning.auth_geo
-ORDER BY random()
-LIMIT 20) j
