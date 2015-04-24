2a051c73d89788363588914faa69ccaca69a79b2
Tom Buckley <buckley.tom@gmail.com>
2015-04-24 15:29:33 -0700

    fix
    trivial changes related to commented out lines and line numbers

--- process/parcel_zoning_intersection.sql
+++ process/parcel_zoning_intersection.sql
@@ -1,8 +1,6 @@
-/*SET UP PARCELS, 
-SELECT VALID GEOMS ONLY, 
-PUT THEM IN EPSG USED FOR ZONING*/
+--SET UP PARCELS, SELECT VALID GEOMS ONLY, PUT THEM IN EPSG USED FOR ZONING
 
-/*ALTER TABLE parcel 
+ALTER TABLE parcel 
    ALTER COLUMN geom 
    TYPE Geometry(MultiPolygon, 26910) 
    USING ST_Transform(geom, 26910);
@@ -26,22 +24,21 @@ WHERE GeometryType(geom) = 'GEOMETRYCOLLECTION';
 CREATE TABLE parcel_valid as 
 SELECT * FROM parcel
 WHERE ST_IsValid(geom) = true;
-*/
-/*SET UP ZONING, 
-SELECT VALID GEOMS ONLY*/
 
-/*CREATE TABLE zoning.lookup_valid AS
+--SET UP ZONING, SELECT VALID GEOMS ONLY
+
+CREATE TABLE zoning.lookup_valid AS
 SELECT 
 	ogc_fid, tablename, ST_MakeValid(the_geom) geom
 FROM
 	zoning.merged_jurisdictions;
 
 CREATE INDEX lookup_2012_valid_gidx ON zoning.lookup_valid USING GIST (geom);
-*/
-CREATE TABLE zoning.merged_jurisdictions_problem_geoms AS
+
+INSERT INTO zoning.lookup_2012_problem_geoms (
 SELECT *
 FROM zoning.lookup_valid
-WHERE GeometryType(geom) = 'GEOMETRYCOLLECTION';
+WHERE GeometryType(geom) = 'GEOMETRYCOLLECTION');
 
 DELETE FROM zoning.lookup_valid
 WHERE GeometryType(geom) = 'GEOMETRYCOLLECTION';
