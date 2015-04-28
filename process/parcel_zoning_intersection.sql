1a04f073c71904d231fb2a4143e43ca44e5ec299
Tom Buckley <buckley.tom@gmail.com>
2015-04-27 22:00:17 -0700

    uncommented parcel/jurisdiction join table creation
    these were needed during the parcel/zoning step that is based on jurisdiction

--- process/parcel_zoning_intersection.sql
+++ process/parcel_zoning_intersection.sql
@@ -63,21 +63,32 @@ CREATE INDEX zoning_bay_area_generic_gidx ON zoning.bay_area_generic USING GIST 
 CREATE INDEX zoning_bay_area_zoning_id_gidx ON zoning.bay_area_generic USING HASH (zoning_id);
 VACUUM (ANALYZE) zoning.bay_area_generic;
 
-/*
+--IN THE FUTURE, THESE SHOULD JUST BE PART OF THE parcel table?
+DROP TABLE IF EXISTS zoning.parcel_counties;
 CREATE TABLE zoning.parcel_counties AS
-SELECT county.name10 as countyname1, county.namelsad10 as countyname2, county.geoid10 countygeoid, p.zoning_id,p.geom_id,p.geom FROM
+SELECT county.name10 as countyname1, county.namelsad10 as countyname2, county.geoid10 countygeoid,p.geom_id, p.geom FROM
 			county10_ca county,
 			parcel p
 			WHERE ST_Intersects(county.wkb_geometry, p.geom);
 --Query returned successfully: 1954393 rows affected, 142212 ms execution time.
 
+DROP INDEX IF EXISTS zoning_parcel_counties_gidx;
+CREATE INDEX zoning_parcel_counties_geomid_idx ON zoning.parcel_counties using GIST (geom);
+VACUUM (ANALYZE) zoning.parcel_counties;
+
+DROP TABLE IF EXISTS zoning_parcel_counties_gidx;
 CREATE TABLE zoning.parcel_cities_counties AS
-SELECT city.name10 as cityname1, city.namelsad10 as cityname2, city.geoid10 citygeoid, p.zoning_id,p.geom_id,p.id,p.geom
+SELECT city.name10 as cityname1, city.namelsad10 as cityname2, city.geoid10 citygeoid, p.geom_id
 FROM 
 city10_ba city,
 zoning.parcel_counties p 
 WHERE ST_Intersects(city.wkb_geometry, p.geom);
-*/
+
+DROP INDEX IF EXISTS zoning_parcel_cities_counties_geomid_idx;
+CREATE INDEX zoning_parcel_cities_counties_geomid_idx ON zoning.parcel_cities_counties using hash (geom_id);
+DROP INDEX IF EXISTS zoning_codes_dictionary_idx;
+CREATE INDEX zoning_parcel_cities_counties_cityname_idx ON zoning.parcel_cities_counties using hash (cityname1);
+VACUUM (ANALYZE) zoning.parcel_cities_counties;
 
 CREATE TABLE zoning.parcel_intersection AS
 SELECT p.geom_id,z.zoning_id FROM
