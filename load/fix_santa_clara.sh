cd2a7e02621d894db24a700cf89eff00513bbbf4
Tom Buckley <buckley.tom@gmail.com>
2015-05-06 14:14:13 -0700

    added boilerplate database names to santa_clara fix
    need these to have the ogr2ogr import work

--- load/fix_santa_clara.sh
+++ load/fix_santa_clara.sh
@@ -1,10 +1,15 @@
+DBUSERNAME=vagrant
+DBPASSWORD=vagrant
+DBHOST=localhost
+DBPORT=5432
+DBNAME=vagrant 
+
 #FIX for Santa Clara
 ogr2ogr -skipfailures -f "PostgreSQL" \
 PG:"host=${DBHOST} port=${DBPORT} dbname=${DBNAME} user=${DBUSERNAME} password=${DBPASSWORD}" -select full_name \
 -nlt PROMOTE_TO_MULTI -lco SCHEMA=zoning_staging -lco OVERWRITE=YES jurisdictional/City_Santa_Clara_GP_LU_02.shp
 
 psql -c <<EOF
-
 update zoning.source_field_name 
 set tablename='City_Santa_Clara_GP_LU_02' 
 where juris=27;
