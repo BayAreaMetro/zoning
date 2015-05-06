a7dd3e4c5b26f4adbadc493c6d5ea23e4d004d78
Tom Buckley <buckley.tom@gmail.com>
2015-05-06 15:52:26 -0700

    Debugging loading santa_clara_city shapefile to database remove select from import for santa clara
    there are only 2 columns anyway, and this one was cut paste from elsewhere
    
    fix directory location for shapefile
    
    no need for -c flag for EOF; close sql

--- load/fix_santa_clara.sh
+++ load/fix_santa_clara.sh
@@ -6,10 +6,10 @@ DBNAME=vagrant
 
 #FIX for Santa Clara
 ogr2ogr -skipfailures -f "PostgreSQL" \
-PG:"host=${DBHOST} port=${DBPORT} dbname=${DBNAME} user=${DBUSERNAME} password=${DBPASSWORD}" -select full_name \
--nlt PROMOTE_TO_MULTI -lco SCHEMA=zoning_staging -lco OVERWRITE=YES jurisdictional/City_Santa_Clara_GP_LU_02.shp
+PG:"host=${DBHOST} port=${DBPORT} dbname=${DBNAME} user=${DBUSERNAME} password=${DBPASSWORD}" \
+-nlt PROMOTE_TO_MULTI -lco SCHEMA=zoning_staging -lco OVERWRITE=YES City_Santa_Clara_GP_LU_02.shp
 
-psql -c <<EOF
+psql <<EOF
 update zoning.source_field_name 
 set tablename='City_Santa_Clara_GP_LU_02' 
 where juris=27;
@@ -31,5 +31,5 @@ City_Santa_Clara_GP_LU_02 z,
 zoning.codes_dictionary c
 WHERE 
 ST_INTERSECTS(z.wkb_geometry,scp.geom) AND
-c.name=z.GP_DESIGNA 
+c.name=z.GP_DESIGNA ;
 EOF
