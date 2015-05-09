874cd8b21d2b6840b276a08e980531339efddf79
Tom Buckley <buckley.tom@gmail.com>
2015-05-09 00:06:35 +0000

    debugging overlaps for santa clara
    minor changes in variable names, syntax, etc, but makes overlaps calculation work

--- load/fix_santa_clara.sh
+++ load/fix_santa_clara.sh
@@ -35,35 +35,37 @@ zoning_staging.City_Santa_Clara_GP_LU_02 z,
 zoning.codes_dictionary c
 WHERE 
 ST_INTERSECTS(z.wkb_geometry,scp.geom) AND
-c.name=z.GP_DESIGNA ;
+c.name=z.GP_DESIGNA;
 
+DROP TABLE IF EXISTS zoning.parcel_intersection_count_santa_clara;
 CREATE TABLE zoning.parcel_intersection_count_santa_clara AS
 SELECT geom_id, count(*) as countof FROM
 			zoning.parcel_intersection_santa_clara
 			GROUP BY geom_id;
 
-CREATE INDEX zoning_parcel_intersection_count ON zoning.parcel_intersection_count_santa_clara (countof);
+CREATE INDEX zoning_parcel_intersection_count_santa_clara ON zoning.parcel_intersection_count_santa_clara (countof);
 VACUUM (ANALYZE) zoning.parcel_intersection_count_santa_clara;
 
 DROP VIEW IF EXISTS zoning.parcels_with_multiple_zoning_santa_clara;
 CREATE VIEW zoning.parcels_with_multiple_zoning_santa_clara AS
 SELECT geom_id, geom from parcel where geom_id
-IN (SELECT geom_id FROM zoning.parcel_intersection_count WHERE countof>1);
+IN (SELECT geom_id FROM zoning.parcel_intersection_count_santa_clara WHERE countof>1);
 
 --read in overlaps function
-\i ../process/get_overlaps_function.sql
+\i process/get_overlaps_function.sql
 
+DROP TABLE IF EXISTS zoning.parcel_overlaps_santa_clara;
 CREATE TABLE zoning.parcel_overlaps_santa_clara AS
-select * from GetOverlaps('zoning.parcels_with_multiple_zoning_santa_clara','zoning_staging.City_Santa_Clara_GP_LU_02','GP_DESIGNA','wkb_geometry') as overlaps(
+select * from GetOverlaps('zoning.parcels_with_multiple_zoning_santa_clara','zoning_staging.City_Santa_Clara_GP_LU_02','GP_DESIGNA','wkb_geometry') as codes(
 		geom_id bigint, 
-  		zoning_id varchar(50),
+  		zoning_string varchar(42),
 		area double precision,
 		prop double precision,
 		geom geometry);
 
 DROP TABLE IF EXISTS zoning.parcel_overlaps_maxonly_santa_clara;
 CREATE TABLE zoning.parcel_overlaps_maxonly_santa_clara AS
-SELECT geom_id, zoning_id, prop 
+SELECT geom_id, zoning_string, prop 
 FROM zoning.parcel_overlaps_santa_clara WHERE (geom_id,prop) IN 
 ( SELECT geom_id, MAX(prop)
   FROM zoning.parcel_overlaps_santa_clara
