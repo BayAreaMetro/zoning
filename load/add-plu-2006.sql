dd724eb01fc617b63110bfd5b9a1504abbbc2361
Tom Buckley <buckley.tom@gmail.com>
2015-05-13 23:50:43 +0000

    add working single-intersection insert of '06 data
    this insert fills 126k parcel zoning details in

--- load/add-plu-2006.sql
+++ load/add-plu-2006.sql
@@ -39,7 +39,7 @@ SELECT geom_id, count(*) as countof FROM
 			zoning.unmapped_parcel_zoning_plu
 			GROUP BY geom_id;
 
-DROP TABLE IF EXISTS zoning.parcel_overlaps_plu 
+DROP TABLE IF EXISTS zoning.parcel_overlaps_plu;
 CREATE TABLE zoning.parcel_overlaps_plu AS
 SELECT 
 	geom_id,
@@ -59,25 +59,47 @@ FROM (
 				(select geom_id 
 					from zoning.unmapped_parcel_intersection_count 
 					WHERE countof>1)) as p,
-				(select origgplu, wkb_geometry from plu06_may2015estimate) as z
+				(select objectid, wkb_geometry from plu06_may2015estimate) as z
 		WHERE ST_Intersects(z.wkb_geometry, p.geom)
 		) f
 GROUP BY 
 	geom_id,
-	origgplu;
+	plu06_objectid;
 
+DROP TABLE IF EXISTS zoning.parcel_withdetails_test
 CREATE TABLE zoning.parcel_withdetails_test
 AS SELECT * from zoning.parcel_withdetails;
 
-INSERT INTO zoning.parcel_withdetails_test
-p.geom_id, -9999 as zoning_id,
-p.hs,p.ht,p.hm,p.of,p.ho,p.sc,p.il,p.iw,p.ih,p.rs,p.rb,p.mr,p.mt,p.me,
-p.max_far,p.max_height,p.max_dua,
-p.max_du_per as max_du_per_parcel
-/*min_far,p.min_front_setback,p.max_front_setback,
-p.side_setback,p.rear_setback,p.min_dua,p.coverage,
-p.min_lot_size*/
+*/
+
+
+INSERT INTO zoning.parcel_withdetails
+SELECT 
+9999 as id, 
+juris as juris, 
+'NA' as city, 
+gengplu as name,
+-9999 as min_far, 
+z.max_height as max_height,
+z.max_far as max_far, 
+-9999 as min_front_setback,
+-9999 as max_front_setback,
+-9999 as side_setback,
+-9999 as rear_setback,
+-9999 as min_dua,
+z.max_dua as max_dua,           
+-9999 as coverage,          
+z.max_du_per as max_du_per_parcel,
+-9999 as min_lot_size,      
+z.hs,z.ht,z.hm,z.of,z.ho,z.sc,z.il,z.iw,z.ih,z.rs,z.rb,z.mr,z.mt,z.me,
+p.geom_id as geom_id,
+p.geom as geom
 from 
+(select * from zoning.unmapped_parcel_zoning_plu
+where geom_id in 
 (select geom_id 
 from zoning.unmapped_parcel_intersection_count 
-WHERE countof=1) as p
+WHERE countof=1)) as p,
+plu06_may2015estimate z
+WHERE p.plu06_objectid=z.objectid;
+
