ce8eb002636cca04febaecfe2c0ffe7b7517a10e
Tom Buckley <buckley.tom@gmail.com>
2015-06-06 19:07:05 -0700

    add source tablename to intersection; put comments next to table creation
    1 will make it easier to tell where an assignment came from
    2 will make it easier to identify what a table is for

--- process/create_intersection_table.sql
+++ process/create_intersection_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE zoning.parcel_intersection AS
-SELECT p.geom_id,z.zoning_id FROM
+SELECT p.geom_id,z.zoning_id, 100 as prop, z.tablename as tablename FROM
 	(select zoning_id, geom from zoning.bay_area_generic) AS z, 
 	(select geom_id, geom from parcel) AS p
 WHERE p.geom && z.geom AND
 ST_Intersects(p.geom,z.geom);
+COMMENT ON TABLE zoning.parcel is 'parcel/zoning intersection output table';
--- process/prepare_parcels.sql
+++ process/prepare_parcels.sql
@@ -7,12 +7,14 @@ CREATE TABLE parcel_invalid AS
 SELECT *
 FROM parcel
 WHERE ST_IsValid(geom) = false;
+COMMENT ON TABLE parcel_invalid is 'spandex parcels-invalid geometries';
 
 CREATE TABLE parcel_geometrycollection
 AS
 SELECT *
 FROM parcel
 WHERE GeometryType(geom) = 'GEOMETRYCOLLECTION';
+COMMENT ON TABLE parcel_geometrycollection is 'spandex parcels-geometrycollection type';
 --returns 0 rows
 
 DELETE FROM parcel
@@ -20,6 +22,7 @@ WHERE GeometryType(geom) = 'GEOMETRYCOLLECTION';
 
 DELETE FROM parcel 
 WHERE ST_IsValid(geom) = false;
+COMMENT ON TABLE parcel_invalid is 'spandex parcels-only valid geometries';
 
 create INDEX parcel_geom_id_idx ON parcel using hash (geom_id);
 CREATE INDEX parcel_gidx ON parcel USING GIST (geom);
