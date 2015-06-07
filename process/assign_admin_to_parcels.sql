74344829f3247accc62e3806225e3e62a9c495d6
Tom Buckley <buckley.tom@gmail.com>
2015-06-07 11:44:09 -0700

    simplify parcel/admin intersection
    previously it was convoluted

--- process/assign_admin_to_parcels.sql
+++ process/assign_admin_to_parcels.sql
@@ -1,19 +1,23 @@
 --IN THE FUTURE, THESE SHOULD JUST BE PART OF THE parcel table?
-DROP TABLE IF EXISTS zoning.parcel_counties;
+DROP TABLE IF EXISTS admin.parcel_counties;
 CREATE TABLE zoning.parcel_counties AS
-SELECT county.name10 as countyname1, county.namelsad10 as countyname2, county.geoid10 countygeoid,p.geom_id, p.geom FROM
-			admin.county10_ca county,
-			parcel p
-			WHERE ST_Intersects(county.geom, p.geom);
---Query returned successfully: 1954393 rows affected, 142212 ms execution time.
+SELECT county.name10 as countyname1, 
+county.namelsad10 as countyname2, 
+county.geoid10 countygeoid, 
+p.geom_id
+FROM
+admin.county10_ca county,
+parcel p
+WHERE ST_Intersects(county.geom, p.geom);
+COMMENT ON TABLE zoning.parcel is 'parcels st_intersect with census 2010 county boundaries';
 
-DROP INDEX IF EXISTS zoning_parcel_counties_gidx;
-CREATE INDEX zoning_parcel_counties_geomid_idx ON zoning.parcel_counties using GIST (geom);
-VACUUM (ANALYZE) zoning.parcel_counties;
-
-DROP TABLE IF EXISTS zoning_parcel_counties;
-CREATE TABLE zoning.parcel_cities_counties AS
-SELECT city.name10 as cityname1, city.namelsad10 as cityname2, city.geoid10 citygeoid, p.geom_id
+DROP TABLE IF EXISTS admin.parcel_cities;
+CREATE TABLE admin.parcel_cities AS
+SELECT city.name10 as cityname1, 
+city.namelsad10 as cityname2, 
+city.geoid10 citygeoid, 
+p.geom_id, 
+p.geom
 FROM 
 admin.city10_ba city,
 zoning.parcel_counties p 
