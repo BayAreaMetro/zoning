40b37915cbdb2a0849f588c44a9a7f54d393dd7f
Tom Buckley <buckley.tom@gmail.com>
2015-06-07 20:49:31 -0700

    fixup

--- process/prepare_zoning.sql
+++ process/prepare_zoning.sql
@@ -4,14 +4,14 @@ SELECT
 	tablename, juris, zoning, ST_MakeValid(the_geom) geom
 FROM
 	zoning.merged_jurisdictions;
-COMMENT ON TABLE zoning.lookup_valid is 'subset of zoning.merged_jurisdictions with valid geometries only';
+COMMENT ON TABLE zoning.bay_area is 'subset of zoning.merged_jurisdictions with valid geometries only';
 
 DROP TABLE zoning.invalid ;
 CREATE TABLE zoning.invalid AS
 SELECT *
 FROM zoning.bay_area
 WHERE ST_IsValid(geom) = false;
-COMMENT ON TABLE zoning.lookup_valid is 'subset of zoning.merged_jurisdictions with invalid geometries only';
+COMMENT ON TABLE zoning.invalid is 'subset of zoning.merged_jurisdictions with invalid geometries only';
 
 DELETE FROM zoning.bay_area 
 WHERE ST_IsValid(geom) = false;
@@ -22,7 +22,7 @@ AS
 SELECT *
 FROM zoning.bay_area
 WHERE GeometryType(geom) = 'GEOMETRYCOLLECTION';
-COMMENT ON TABLE zoning.lookup_valid is 'subset of zoning.merged_jurisdictions with geometry collection geometries only';
+COMMENT ON TABLE zoning.geometry_collection is 'subset of zoning.merged_jurisdictions with geometry collection geometries only';
 
 DELETE FROM zoning.bay_area
 WHERE GeometryType(geom) = 'GEOMETRYCOLLECTION';
@@ -34,7 +34,7 @@ zoning.codes_dictionary c,
 zoning.bay_area z
 WHERE c.juris=z.juris 
 AND c.name = z.zoning;
-COMMENT ON TABLE zoning.lookup_valid is 'merged bay area zoning joined with generic code table';
+COMMENT ON TABLE zoning.bay_area_generic is 'merged bay area zoning joined with generic code table';
 
 CREATE INDEX zoning_bay_area_generic_gidx ON zoning.bay_area_generic USING GIST (geom);
 CREATE INDEX zoning_bay_area_zoning_id_gidx ON zoning.bay_area_generic USING HASH (zoning_id);
