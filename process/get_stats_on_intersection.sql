aad149cc52a9d7f9ce881cc6be54df82343b485e
Tom Buckley <buckley.tom@gmail.com>
2015-06-08 04:48:34 +0000

    fixups on get_stats_on_intersection

--- process/get_stats_on_intersection.sql
+++ process/get_stats_on_intersection.sql
@@ -1,16 +1,18 @@
+DROP TABLE IF EXISTS zoning.parcel_intersection_count cascade;
 CREATE TABLE zoning.parcel_intersection_count AS
 SELECT geom_id, count(*) as countof FROM
 			zoning.parcel_intersection
 			GROUP BY geom_id;
 COMMENT ON TABLE zoning.parcel_intersection_count is 'count by geom_id of st_intersects of parcel and zoning';
 
-#add table to inspect errors in overlapping areas
+--add table to inspect errors in overlapping areas
 CREATE INDEX zoning_parcel_intersection_count_geom_id ON zoning.parcel_intersection_count (geom_id);
 VACUUM (ANALYZE) zoning.parcel_intersection_count;
 
+DROP TABLE IF EXISTS zoning.parcel_intersection_count_geo;
 CREATE TABLE zoning.parcel_intersection_count_geo AS
-SELECT c.countof, p.geom_id, p.geom from parcel2 p, zoning.parcel_intersection_count c  where p.geom_id=c.geom_id
-COMMENT ON TABLE zoning.parcel_intersection_count is 'count by geom_id of st_intersects of parcel and zoning with geometry';
+SELECT c.countof, p.geom_id, p.geom from parcel p, zoning.parcel_intersection_count c  where p.geom_id=c.geom_id;
+COMMENT ON TABLE zoning.parcel_intersection_count_geo is 'count by geom_id of st_intersects of parcel and zoning with geometry';
 
 ALTER TABLE zoning.parcel_intersection_count_geo ADD PRIMARY KEY (geom_id);
 
@@ -21,5 +23,5 @@ DROP VIEW IF EXISTS zoning.parcels_with_multiple_zoning;
 CREATE VIEW zoning.parcels_with_multiple_zoning AS
 SELECT geom_id, geom from parcel where geom_id
 IN (SELECT geom_id FROM zoning.parcel_intersection_count WHERE countof>1);
-COMMENT ON VIEW zoning.parcel_intersection_count is 'parcels that have more than 1 intersecting zoning multipolygon';
+COMMENT ON VIEW zoning.parcels_with_multiple_zoning is 'parcels that have more than 1 intersecting zoning multipolygon';
 
