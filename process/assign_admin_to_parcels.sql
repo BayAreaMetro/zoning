5eae44664a82615adc9084faaff41cc56cf769cd
Tom Buckley <buckley.tom@gmail.com>
2015-06-07 17:28:08 -0700

    improve indexes on admin/parcel tables

--- process/assign_admin_to_parcels.sql
+++ process/assign_admin_to_parcels.sql
@@ -1,8 +1,8 @@
 --IN THE FUTURE, THESE SHOULD JUST BE PART OF THE parcel table?
 DROP TABLE IF EXISTS admin.parcel_counties;
 CREATE TABLE zoning.parcel_counties AS
-SELECT county.name10 as countyname1, 
-county.namelsad10 as countyname2, 
+SELECT county.name10 as name1, 
+county.namelsad10 as name2, 
 county.geoid10 countygeoid, 
 p.geom_id
 FROM
@@ -11,10 +11,18 @@ parcel p
 WHERE ST_Intersects(county.geom, p.geom);
 COMMENT ON TABLE admin.parcel is 'parcels st_intersect with census 2010 county boundaries';
 
+DROP INDEX IF EXISTS zoning_parcels_counties_geomid_idx;
+CREATE INDEX zoning_parcels_counties_geomid_idx ON zoning.parcel_counties using hash (geom_id);
+
+DROP INDEX IF EXISTS zoning_parcel_counties_name_idx;
+CREATE INDEX zoning_parcel_counties_name_idx ON zoning.parcel_counties using hash (name1);
+
+VACUUM (ANALYZE) zoning.parcel_counties;
+
 DROP TABLE IF EXISTS admin.parcel_cities;
 CREATE TABLE admin.parcel_cities AS
-SELECT city.name10 as cityname1, 
-city.namelsad10 as cityname2, 
+SELECT city.name10 as name1, 
+city.namelsad10 as name2, 
 city.geoid10 citygeoid, 
 p.geom_id, 
 p.geom
@@ -22,11 +30,12 @@ FROM
 admin.city10_ba city,
 zoning.parcel p 
 WHERE ST_Intersects(city.geom, p.geom);
+COMMENT ON TABLE admin.parcel is 'parcels st_intersect with census 2010 city boundaries';
 
-DROP INDEX IF EXISTS zoning_parcels_counties_geomid_idx;
-CREATE INDEX zoning_parcels_counties_geomid_idx ON zoning.parcel_cities_counties using hash (geom_id);
-DROP INDEX IF EXISTS zoning_codes_dictionary_idx;
-CREATE INDEX zoning_parcel_cities_cityname_idx ON zoning.parcel_cities_counties using hash (cityname1);
-VACUUM (ANALYZE) zoning.parcel_cities_counties;
-COMMENT ON TABLE zoning.parcel is 'parcels st_intersect with census 2010 city boundaries';
+DROP INDEX IF EXISTS zoning_parcels_cities_geomid_idx;
+CREATE INDEX zoning_parcels_cities_geomid_idx ON zoning.parcel_cities using hash (geom_id);
 
+DROP INDEX IF EXISTS zoning_parcel_cities_name_idx;
+CREATE INDEX zoning_parcel_cities_name_idx ON zoning.parcel_cities using hash (name1);
+
+VACUUM (ANALYZE) zoning.parcel_cities;
