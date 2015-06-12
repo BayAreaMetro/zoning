8b03818fea0fdfca0d765e5d277a966458194f5d
Tom Buckley <buckley.tom@gmail.com>
2015-06-12 10:37:31 -0700

    modify plu06 cleaning script for different columns
    the plu '06 file contains many more columns than the zoning tables we borrowed this sciprt from. so in this case we clean them by updating the column rather than creating a new table.

--- process/clean_plu06_geoms.sql
+++ process/clean_plu06_geoms.sql
@@ -1,10 +1,11 @@
-DROP TABLE zoning.plu06_may2015estimate_valid;
+/*DROP TABLE zoning.plu06_may2015estimate_valid;
 CREATE TABLE zoning.plu06_may2015estimate_valid AS
 SELECT 
-	tablename, juris, zoning, ST_MakeValid(geom) geom
+	juris, zoning, ST_MakeValid(geom) geom
 FROM
 	zoning.plu06_may2015estimate;
 COMMENT ON TABLE zoning.plu06_may2015estimate_valid is 'subset of zoning.plu06_may2015estimate_source with valid geometries only';
+*/
 
 DROP TABLE zoning.plu06_may2015estimate_invalid;
 CREATE TABLE zoning.plu06_may2015estimate_invalid AS
@@ -13,20 +14,20 @@ FROM zoning.plu06_may2015estimate
 WHERE ST_IsValid(geom) = false;
 COMMENT ON TABLE zoning.plu06_may2015estimate_invalid is 'subset of zoning.plu06_may2015estimate_source with invalid geometries only';
 
+UPDATE zoning.plu06_may2015estimate
+	SET geom = ST_MakeValid(geom);
+
 DROP TABLE zoning.plu06_may2015estimate_geometry_collection;
 CREATE TABLE zoning.plu06_may2015estimate_geometry_collection
 AS
 SELECT *
-FROM zoning.plu06_may2015estimate_valid
+FROM zoning.plu06_may2015estimate
 WHERE GeometryType(geom) <> 'MULTIPOLYGON';
 COMMENT ON TABLE zoning.geometry_collection is 'subset of zoning.plu06_may2015estimate_source with non multipolygon geometries produced by makevalid';
 
-DELETE FROM zoning.plu06_may2015estimate_valid
+DELETE FROM zoning.plu06_may2015estimate
 WHERE GeometryType(geom) <> 'MULTIPOLYGON';
 
 SELECT UpdateGeometrySRID('zoning','plu06_may2015estimate_valid','geom',26910);
-ALTER TABLE zoning.plu06_may2015estimate_valid 
+ALTER TABLE zoning.plu06_may2015estimate
  ALTER COLUMN geom TYPE geometry(MULTIPOLYGON, 26910);
-
-ALTER TABLE zoning.plu06_may2015estimate RENAME TO plu06_may2015estimate_source;
-COMMENT ON TABLE zoning.plu06_may2015estimate_source is 'use zoning.plu06_may2015estimate_valid for working multipolygons.';
