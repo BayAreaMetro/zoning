da44c27510afd2b44df8993fd3db134a9c4b35f6
Tom Buckley <buckley.tom@gmail.com>
2015-06-11 17:08:40 -0700

    copy insert template from cities to counties

--- process/assign_zoning_to_parcels_in_unincorporated.sql
+++ process/assign_zoning_to_parcels_in_unincorporated.sql
@@ -42,3 +42,29 @@ DROP INDEX zoning_contested_parcel_in_counties_multiple_max;
 CREATE INDEX zoning_contested_parcel_in_counties_multiple_max ON zoning.contested_parcel_in_counties_multiple_max using GIST (geom);
 
 vacuum (analyze) zoning.contested_parcel_in_counties_multiple_max
+
+DROP TABLE IF EXISTS zoning.parcel_geo3;
+CREATE TABLE zoning.parcel_geo3 AS
+SELECT *
+FROM zoning.contested_parcel_in_counties_single_max;
+COMMENT ON TABLE zoning.parcel_geo1 is 'A geo-table of parcel intersection from counties'
+
+CREATE INDEX zoning_parcel_geo2_gidx ON zoning.parcel_geo2 using GIST (geom);
+vacuum (analyze) zoning.parcel_geo2;
+
+DROP TABLE IF EXISTS zoning.parcel_contested3;
+CREATE TABLE zoning.parcel_contested3 AS
+SELECT *
+FROM parcel
+WHERE geom_id NOT IN (SELECT geom_id FROM zoning.parcel);
+COMMENT ON TABLE zoning.parcel_contested2 is 'A geo-table of parcels with more than 1 intersection, and not in city intersection or county intersection based on 2011 data. ';
+
+CREATE INDEX zoning_parcel_contested_gidx ON zoning.parcel_contested2 using GIST (geom);
+
+vacuum (analyze) zoning.parcel_contested3;
+vacuum (analyze) zoning.parcel_geo3;
+
+INSERT INTO zoning.parcel 
+SELECT geom_id, zoning_id, prop, tablename from 
+zoning.contested_parcel_in_counties_single_max;
+SELECT COUNT(geom_id) - COUNT(DISTINCT geom_id) FROM zoning.parcel;
