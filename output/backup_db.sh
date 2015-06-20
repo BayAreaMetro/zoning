aaac5da64b7a14cac7ab5776505f4294ab4d55fe
Tom Buckley <buckley.tom@gmail.com>
2015-06-20 02:39:35 +0000

    backup the database make task
    this backs up the database and pushes it to s3. we do this because we might need to refer back to some aspect of how the parcels were generated.

--- output/backup_db.sh
+++ output/backup_db.sh
@@ -1,12 +1,12 @@
-#from http://stackoverflow.com/questions/3518218/moving-a-database-with-pg-dump-and-psql-u-postgres-db-name-results-in-er
-#Dump all global objects, such as users and group
-pg_dumpall -g -U postgres > /vm_project_dir/globals.sql
+# from http://stackoverflow.com/questions/3518218/moving-a-database-with-pg-dump-and-psql-u-postgres-db-name-results-in-er
+# Dump all global objects, such as users and group
+sudo -u postgres pg_dumpall -g -U postgres > /vm_project_dir/globals.sql
 
-#Dump schema of database:
-pg_dump -Fp -s -v -f /vm_project_dir/db-schema.sql -U postgres mtc
+# #Dump schema of database:
+sudo -u postgres pg_dump -Fp -s -v -f /vm_project_dir/db-schema.sql -U postgres mtc
 
 #Dump contents of database:
-pg_dump -Fc -v -f /vm_project_dir/full.dump -U postgres mtc
+sudo -u postgres pg_dump -Fc -v -f /vm_project_dir/full.dump -U postgres mtc
 
 #to restore:
 
@@ -16,6 +16,6 @@ pg_dump -Fc -v -f /vm_project_dir/full.dump -U postgres mtc
 
 #write to s3
 today="$(date +'%Y/%m/%d')"
-aws s3 cp /vm_project_dir/globals.sql s3://landuse/spandex/outputs/${today}/globals.sql
+#aws s3 cp /vm_project_dir/globals.sql s3://landuse/spandex/outputs/${today}/globals.sql
 aws s3 cp /vm_project_dir/full.dump s3://landuse/spandex/outputs/${today}/full.dump
-aws s3 cp /vm_project_dir/db-schema.sql s3://landuse/spandex/outputs/${today}/db-schema.sql
+#aws s3 cp /vm_project_dir/db-schema.sql s3://landuse/spandex/outputs/${today}/db-schema.sql
