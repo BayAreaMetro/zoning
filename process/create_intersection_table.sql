c2547ec53598c1868a748bc31da8331687020b7f
Tom Buckley <buckley.tom@gmail.com>
2015-06-22 23:00:26 +0000

    carry source zoning string and juris through into spatial intersection
    previously, we had just used the zoning_id to convey this information, but by carrying these through into the joined table, we can make changes in the zoning_id table and apply those without having to perform a spatial intersection. however, for these changes to show up in tables later, we will need to at some point lookup the id again. probably during assignment?

--- process/create_intersection_table.sql
+++ process/create_intersection_table.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS zoning.parcel_intersection;
 CREATE TABLE zoning.parcel_intersection AS
-SELECT p.geom_id,z.zoning_id, 100 as prop, z.tablename as tablename FROM
-	(select zoning.get_id(zoning,juris) as zoning_id, tablename, geom from zoning.bay_area) AS z, 
+SELECT z.zoning, z.juris, p.geom_id,z.zoning_id, 100 as prop, z.tablename as tablename FROM
+	(select zoning.get_id(zoning,juris) as zoning_id, zoning, juris, tablename, geom from zoning.bay_area) AS z, 
 	(select geom_id, geom from parcel) AS p
 WHERE p.geom && z.geom AND
 ST_Intersects(p.geom,z.geom);
