04ce9063b53256b940a0dda16b31daed3e44bd46
Tom Buckley <buckley.tom@gmail.com>
2015-07-16 01:02:03 +0000

    refactor plu '06 additions for 2-table data setup
    we are doing this because its easier to edit zoning characteristics directly with a 2 table approach. the single table approach was done because we did not have plu '06 codes in the generic zoning table. however, this was easily resolved by adding them from the plu '06 source data. this generic zoning table now exists in bayarea_urbansim

--- process/assign_plu06_to_parcels.sql
+++ process/assign_plu06_to_parcels.sql
@@ -10,9 +10,9 @@ FROM zoning.parcel_overlaps_plu WHERE (geom_id,prop) IN
 DROP TABLE IF EXISTS zoning.plu06_one_intersection; 
 CREATE TABLE zoning.plu06_one_intersection AS
 SELECT
-z.geom_id,
-'6' || lpad(cast(plu06_objectid as text),4,'0000') as z.zoning_id,
-p.prop,
+p.geom_id,
+'6' || lpad(cast(z.objectid as text),4,'0000') as zoning_id,
+100 as prop,
 'plu06' as tablename,
 p.geom
 from 
@@ -29,35 +29,20 @@ vacuum (analyze) zoning.plu06_one_intersection;
 
 DROP TABLE IF EXISTS zoning.plu06_many_intersection;
 CREATE TABLE zoning.plu06_many_intersection AS
-SELECT 
-9999 as id, 
-juris as juris, 
-text 'NA' as city,
-text 'PLU06' as tablename,
-gengplu as name,
--9999 as min_far, 
-z.max_height as max_height,
-z.max_far as max_far, 
--9999 as min_front_setback,
--9999 as max_front_setback,
--9999 as side_setback,
--9999 as rear_setback,
--9999 as min_dua,
-z.max_dua as max_dua,           
--9999 as coverage,          
-z.max_du_per as max_du_per_parcel,
--9999 as min_lot_size,      
-z.hs,z.ht,z.hm,z.of,z.ho,z.sc,z.il,z.iw,z.ih,z.rs,z.rb,z.mr,z.mt,z.me,
-p.geom_id as geom_id,
-p.geom as geom
-from (select p2.* 
+SELECT
+p.geom_id,
+'6' || lpad(cast(z.objectid as text),4,'0000') as zoning_id,
+p.prop,
+'plu06' as tablename,
+p.geom
+from (select p2.*, pmax.prop 
 	from zoning.unmapped_parcel_zoning_plu p2,
 	zoning.parcel_overlaps_maxonly_plu pmax
 	where pmax.geom_id=p2.geom_id AND
 	p2.plu06_objectid=pmax.plu06_objectid) as p,
 zoning.plu06_may2015estimate z
 WHERE p.plu06_objectid=z.objectid;
-COMMENT ON TABLE zoning.plu06_many_intersection IS 'plu 06 intersection table with selected greatest max value of zoning'
+COMMENT ON TABLE zoning.plu06_many_intersection IS 'plu 06 intersection table with selected greatest max value of zoning';
 
 create INDEX plu06_many_intersection_gidx ON zoning.plu06_many_intersection using GIST (geom);
 vacuum (analyze) zoning.plu06_one_intersection;
@@ -90,49 +75,18 @@ zoning.plu06_many_intersection where (geom_id) IN
 	WHERE b.countof>1
 	);
 
-DROP TABLE IF EXISTS zoning.parcel_withdetails;
-CREATE TABLE zoning.parcel_withdetails AS
-SELECT z.id, 
-z.juris, 
-z.city,
-pz.tablename,
-z.name,
-z.min_far, 
-z.max_height,
-z.max_far, 
-z.min_front_setback,
-z.max_front_setback,
-z.side_setback,
-z.rear_setback,
-z.min_dua,
-z.max_dua,           
-z.coverage,          
-z.max_du_per_parcel,
-z.min_lot_size,      
-z.hs,z.ht,z.hm,z.of,z.ho,z.sc,z.il,z.iw,z.ih,z.rs,z.rb,z.mr,z.mt,z.me,
-p.geom_id as geom_id,
-p.geom as geom
-FROM zoning.parcel pz,
-zoning.codes_dictionary z,
-parcel p
-WHERE pz.zoning_id = z.id AND p.geom_id = pz.geom_id;
-
-CREATE INDEX zoning_parcel_withdetails_gidx ON zoning.parcel_withdetails using GIST (geom);
-CREATE INDEX zoning_parcel_withdetails_geom_idx ON zoning.parcel_withdetails using hash (geom_id);
-VACUUM (ANALYZE) zoning.parcel_withdetails;
-
-INSERT INTO zoning.parcel_withdetails
+INSERT INTO zoning.parcel
 select * from zoning.plu06_one_intersection
-WHERE geom_id NOT IN (SELECT geom_id from zoning.parcel_withdetails);
-SELECT COUNT(geom_id) - COUNT(DISTINCT geom_id) FROM zoning.parcel_withdetails;
+WHERE geom_id NOT IN (SELECT geom_id from zoning.parcel);
+SELECT COUNT(geom_id) - COUNT(DISTINCT geom_id) FROM zoning.parcel;
 
-VACUUM (ANALYZE) zoning.parcel_withdetails;
+VACUUM (ANALYZE) zoning.parcel;
 
-INSERT INTO zoning.parcel_withdetails
+INSERT INTO zoning.parcel
 select * from zoning.plu06_many_intersection
-WHERE geom_id NOT IN (SELECT geom_id from zoning.parcel_withdetails);
-SELECT COUNT(geom_id) - COUNT(DISTINCT geom_id) FROM zoning.parcel_withdetails;
+WHERE geom_id NOT IN (SELECT geom_id from zoning.parcel);
+SELECT COUNT(geom_id) - COUNT(DISTINCT geom_id) FROM zoning.parcel;
 
-VACUUM (ANALYZE) zoning.parcel_withdetails;
+VACUUM (ANALYZE) zoning.parcel;
 
-ALTER TABLE zoning.parcel_withdetails ADD COLUMN nodev integer NOT NULL DEFAULT 0;
+ALTER TABLE zoning.parcel ADD COLUMN nodev integer NOT NULL DEFAULT 0;
