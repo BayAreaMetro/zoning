9f88947d7ece2494d9bb199247db3a4437e9e0cc
Tom Buckley <buckley.tom@gmail.com>
2015-07-16 02:19:31 +0000

    update nodev for 2 table system
    instead of outputting a whole row of 0's for zoning, we will either put 0's in the zoning_lookup.csv or just use to nodev column here. since the latter preserves information, i am keeping a table like that for now and creating another in which the information about the zoning_id of a nodev parcel is removed."

--- process/apply_no_dev.sql
+++ process/apply_no_dev.sql
@@ -1,26 +1,21 @@
-UPDATE zoning.parcel_withdetails as z
+ALTER TABLE zoning.parcel ADD COLUMN nodev smallint NOT NULL DEFAULT 0;
+
+UPDATE zoning.parcel as z
 SET 
-id = 0000,
-juris = 0000,
-city = z.city,
-tablename = z.tablename,
-name = z.name,
-min_far = 0,
-max_height = 0,
-max_far = 0,
-min_front_setback = 0,
-max_front_setback = 0,
-side_setback = 0,
-rear_setback = 0,
-min_dua = 0,
-max_dua = 0,
-coverage = 0,
-max_du_per_parcel = 0,
-min_lot_size = 0,
-hs = 0,ht = 0,hm = 0,of = 0,ho = 0,sc = 0,il = 0,iw = 0,ih = 0,rs = 0,rb = 0,mr = 0,mt = 0,me = 0,
-geom_id = z.geom_id,
-geom = z.geom,
 nodev = 1
 FROM 
-no_dev_source nd
-WHERE ST_Within(nd.centroid,z.geom);
+no_dev_source nd,
+parcel p
+WHERE 
+p.geom_id = z.geom_id
+AND ST_Within(nd.centroid,p.geom);
+SELECT COUNT(*) from zoning.parcel where nodev=1;
+
+DROP TABLE IF EXISTS zoning.parcel_nodev_remove_zoning_id;
+CREATE TABLE zoning.parcel_nodev_remove_zoning_id AS
+SELECT * FROM zoning.parcel;
+comment on table zoning.parcel_nodev_remove_zoning_id is 'version of zoning.parcel where zoning_id has been changed to 00000 if nodev=1';
+
+UPDATE zoning.parcel_nodev_remove_zoning_id SET
+zoning_id = 00000 
+WHERE nodev = 1;
