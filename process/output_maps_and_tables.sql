679e9ddacb6262f708b25c02cecfb9cb73357de1
Tom Buckley <buckley.tom@gmail.com>
2015-10-06 00:00:06 +0000

    output zoning names undefined in lookup
    we will put these out in order to know which zoning names, from which cities, we need to look up and fill in for the zoning_lookup.csv in bayarea_urbansim.

--- process/output_maps_and_tables.sql
+++ process/output_maps_and_tables.sql
@@ -15,3 +15,7 @@
 
 #\COPY zoning.parcel TO '/vm_project_dir/zoning/zoning_parcels.csv' DELIMITER ',' CSV HEADER;
 \COPY zoning.parcel_nodev_remove_zoning_id TO '/vm_project_dir/zoning/zoning_parcels.csv' DELIMITER ',' CSV HEADER;
+
+RAISE NOTICE 'These are the zoning codes for which we do not have generic definition:';
+select distinct(zoning), tablename from zoning.parcel where geom_id not in (select geom_id from zoning.parcel zp, zoning.codes_dictionary zc
+where zc.name=zp.zoning AND zp.juris=zc.juris);
