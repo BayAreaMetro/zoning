551b08ec66a4a79e8ffec45f688365ccb0b35d86
Tom Buckley <buckley.tom@gmail.com>
2015-10-06 23:27:37 +0000

    exclude plu06 from missing generic list
    plu06 data don't have a juris so get included in here otherwise. they dont have a juris code b/c that code is from the 2012 process, not used in the plu06 process.

--- process/output_maps_and_tables.sql
+++ process/output_maps_and_tables.sql
@@ -18,4 +18,4 @@
 
 RAISE NOTICE 'These are the zoning codes for which we do not have generic definition:';
 select distinct(zoning), tablename from zoning.parcel where geom_id not in (select geom_id from zoning.parcel zp, zoning.codes_dictionary zc
-where zc.name=zp.zoning AND zp.juris=zc.juris);
+where zc.name=zp.zoning AND zp.juris=zc.juris) AND tablename != 'plu06' ORDER BY tablename;
