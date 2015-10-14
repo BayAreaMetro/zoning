df3a93e922e1b1bb45a8e460154d1533e0c0def9
Tom Buckley <buckley.tom@gmail.com>
2015-10-13 23:17:33 -0700

    change layers around for intensities
    whereas previously the zoning map looked at types only, now we are interested in the intensities of specific types. this is a draft look at those together on one map

--- js/main.js
+++ js/main.js
@@ -4,14 +4,45 @@ var projectionExtent = projection.getExtent();
 
 function init(){
 
-var map = L.map('map').setView([37.7, -122.4], 11);
-map.options.maxZoom = 15;map.options.minZoom = 12
+var map = L.map('map').setView([37.7, -122.4], 10);
+map.options.maxZoom = 15;map.options.minZoom = 10
 var toner = new L.StamenTileLayer("toner");
-map.addLayer(toner);
-
 var osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 var bing = new L.BingLayer("Ak9B0icHsz6Z-MEMGpXFxHjFA6liDJYmv2JNiddSzwIK5krb37s03SwekohbbNOs");
+
+map.addLayer(toner);
 map.addLayer(bing);
+
+var multifamily = L.tileLayer('http://s3-us-west-2.amazonaws.com/landuse/zoning/effective_max_dua/{z}/{x}/{y}.png',{tms: true, opacity:0.5}).addTo(map);
+var mixedretail = L.tileLayer('http://s3-us-west-2.amazonaws.com/landuse/zoning/effective_max_far/{z}/{x}/{y}.png',{tms: true, opacity:0.5}).addTo(map);
+
+/*document.getElementById('bing').onclick = function () {
+    var enable = this.className !== 'active';
+    bing.setOpacity(enable ? 1 : 0);
+    this.className = enable ? 'active' : '';
+    return false;
+};*/
+
+document.getElementById('toner').onclick = function () {
+    var enable = this.className !== 'active';
+    toner.setOpacity(enable ? 1 : 0);
+    this.className = enable ? 'active' : '';
+    return false;
+};
+
+document.getElementById('multifamily').onclick = function () {
+    var enable = this.className !== 'active';
+    multifamily.setOpacity(enable ? 0.5 : 0);
+    this.className = enable ? 'active' : '';
+    return false;
+};
+
+document.getElementById('mixedretail').onclick = function () {
+    var enable = this.className !== 'active';
+    mixedretail.setOpacity(enable ? 0.5 : 0);
+    this.className = enable ? 'active' : '';
+    return false;
+};
 
 /*var zoning_wms = L.tileLayer.wms("http://10.1.1.204:8080/geoserver/gwc/service/wms?", {
     layers: 'mtc:all_codes_colors',
@@ -23,15 +54,8 @@ map.addLayer(bing);
 });
 zoning_wms.addTo(map);*/
 
-var multifamily = L.tileLayer.wms('http://10.1.1.204:8080/geoserver/gwc/service/wms?', {
-    layers: 'mtc:all_codes_colors',
-    styles: 'mtc:hm-t',
-    format: 'image/png',
-    transparent: true,
-    version: '1.1.0',
-    attribution: "MTC"
-}).addTo(map);
 
+/*
 var mixedemployment = L.tileLayer.wms('http://10.1.1.204:8080/geoserver/gwc/service/wms?', {
     layers: 'mtc:all_codes_colors',
     styles: 'mtc:me-t',
@@ -103,36 +127,11 @@ var singlefamilyattached = L.tileLayer.wms('http://10.1.1.204:8080/geoserver/gwc
     version: '1.1.0',
     attribution: "MTC"
 }).addTo(map);
-
+*/
 // Layer switcher
-document.getElementById('multifamily').onclick = function () {
-    var enable = this.className !== 'active';
-    multifamily.setOpacity(enable ? 1 : 0);
-    this.className = enable ? 'active' : '';
-    return false;
-};
 
-document.getElementById('mixedemployment').onclick = function () {
-    var enable = this.className !== 'active';
-    mixedemployment.setOpacity(enable ? 1 : 0);
-    this.className = enable ? 'active' : '';
-    return false;
-};
 
-document.getElementById('mixedresidential').onclick = function () {
-    var enable = this.className !== 'active';
-    mixedresidential.setOpacity(enable ? 1 : 0);
-    this.className = enable ? 'active' : '';
-    return false;
-};
-
-document.getElementById('mixedretail').onclick = function () {
-    var enable = this.className !== 'active';
-    mixedretail.setOpacity(enable ? 1 : 0);
-    this.className = enable ? 'active' : '';
-    return false;
-};
-
+/*
 document.getElementById('singlefamilydetached').onclick = function () {
     var enable = this.className !== 'active';
     singlefamilydetached.setOpacity(enable ? 1 : 0);
@@ -168,19 +167,7 @@ document.getElementById('singlefamilyattached').onclick = function () {
     return false;
 };
 
-document.getElementById('bing').onclick = function () {
-    var enable = this.className !== 'active';
-    bing.setOpacity(enable ? 1 : 0);
-    this.className = enable ? 'active' : '';
-    return false;
-};
 
-document.getElementById('toner').onclick = function () {
-    var enable = this.className !== 'active';
-    toner.setOpacity(enable ? 1 : 0);
-    this.className = enable ? 'active' : '';
-    return false;
-};
-
+*/
 
 }
