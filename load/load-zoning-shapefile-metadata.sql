0fe7038b7328ac1fc564ba99617361e3de519aa6
Tom Buckley <buckley.tom@gmail.com>
2016-02-03 23:28:05 +0000

    update merge schema to reflect changes to metadata/2012 data
    small changes to variables names to be consistent with new metadata file and general schema

--- load/load-zoning-shapefile-metadata.sql
+++ load/load-zoning-shapefile-metadata.sql
@@ -6,7 +6,8 @@ CREATE TABLE zoning_staging.shapefile_metadata (
     collection_project_year numeric(4,0), 
     regulation_type text, 
     year_in_tablename numeric(4,0),
-    county boolean
+    county boolean,
+    matchfield text
 );
 
 \COPY zoning_staging.shapefile_metadata FROM 'data/zoning_source_metadata.csv' WITH (FORMAT csv, DELIMITER ',', HEADER FALSE);
