083516bcab8780d5b72c7a317f89eb0be45d51dd
Tom Buckley <buckley.tom@gmail.com>
2016-02-03 23:28:05 +0000

    update merge schema to reflect changes to metadata/2012 data
    small changes to variables names to be consistent with new metadata file and general schema

--- functions/merge_schema.sql
+++ functions/merge_schema.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION zoning.merge(schema_name text)
+CREATE OR REPLACE FUNCTION merge_schema(schema_name text)
    RETURNS void AS
 $BODY$
 DECLARE
@@ -10,26 +10,26 @@ DECLARE
 		  FROM information_schema.tables
 		  WHERE table_schema = $1));
 		sql_string text := '';
-BEGIN
+BEGIN	
 	 FOR table_record IN tables LOOP
 	 sql_string := (SELECT '
-		 insert into zoning.' 
+		 insert into public.' 
 		 || $1 || 
 		 '_merged select ' ||
 		 quote_LITERAL(table_record."table_name") || 
 		 ', CAST(' ||
 		 qry.matchfield ||    
 		 ' as text) as zoning, ' ||
-	 	 qry.juris ||
+	 	 qry.juris_id ||
 		 ' as juris,' ||
 		 'ST_Force2D(geom) as the_geom ' ||
 		 'from ' || $1 || '.' ||
 		 table_record."table_name"		 
 		FROM
 			(
-				select substring(matchfield from 1 for 10) as matchfield, CAST(juris as text)
-					FROM zoning.source_field_name s
-				WHERE s.tablename = table_record."table_name") qry
+				select substring(matchfield from 1 for 10) as matchfield, CAST(juris_id as text)
+					FROM zoning_staging.shapefile_metadata s
+				WHERE s.shapefile_name = table_record."table_name") qry
 			);
 	 	RAISE NOTICE '%', sql_string;
 	 	EXECUTE sql_string;
--- process/merge_2012_zoning.sql
+++ process/merge_2012_zoning.sql
@@ -1,14 +1,11 @@
-DROP TABLE IF EXISTS zoning.zoning_staging_merged;
-CREATE TABLE zoning.zoning_staging_merged
+DROP TABLE IF EXISTS public.zoning_2012_staging_merged;
+CREATE TABLE public.zoning_2012_staging_merged
 (
-  tablename text,
+  shapefile_name text,
   zoning text,
-  juris integer,
+  juris_id integer,
   the_geom geometry(Multipolygon,26910)
 );
 
-select zoning.merge('zoning_staging');
+select merge_schema('zoning_2012_staging');
 
-DROP TABLE IF EXISTS zoning.merged_jurisdictions;
-ALTER TABLE zoning.zoning_staging_merged
-    RENAME TO merged_jurisdictions;
