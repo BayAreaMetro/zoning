62d5f8dc2308fe5f552afd672660048160bb0f83
Tom Buckley <buckley.tom@gmail.com>
2016-02-03 17:22:43 -0800

    assign geoids to zoning 2012 metadata
    we can use this to break zoning out by county through the jurisdiction table

---
+++ process/assign_census_id_to_source_zoning.sql
@@ -0,0 +1,21 @@
+ALTER TABLE parcel
+    ADD COLUMN zoning_id integer;
+
+ALTER TABLE zoning_staging.shapefile_metadata
+    ADD COLUMN geoid10_int integer;
+
+UPDATE zoning_staging.shapefile_metadata m
+    SET geoid10_int = j.geoid10_int
+FROM
+   admin_staging.jurisdictions j
+WHERE j.county=false AND 
+m.county=false AND
+j.name10=m.common_name;
+
+UPDATE zoning_staging.shapefile_metadata m
+    SET geoid10_int = j.geoid10_int
+FROM
+   admin_staging.jurisdictions j
+WHERE j.county=true 
+AND m.county=true AND
+j.name10=m.common_name;
