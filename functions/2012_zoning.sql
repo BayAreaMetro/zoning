740eda4b12dd694942e3928641d3dd5eedf177ba
Tom Buckley <buckley.tom@gmail.com>
2016-02-05 15:23:50 -0800

    add overlaps function for 2012 tables, consolidate
    cleaned up the overlaps function for 2012 tables and consolidated it with the geometry cleaning so we can register them with one command line call.

--- functions/2012_zoning.sql
+++ functions/2012_zoning.sql
@@ -33,6 +33,43 @@ END;
 $function$
 LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION assign_2012(exec boolean = FALSE) RETURNS text AS $function$
+DECLARE
+        sql_string text := '';
+BEGIN
+     SELECT array_to_string(array_agg(sql_statement), '') INTO sql_string
+     FROM (
+        SELECT 'CREATE TABLE
+         zoning_2012_parcel_overlaps.'
+         || qry.shapefile_name ||
+         ' AS SELECT * from GetOverlapsGoeid(' ||
+         qry.geoid10_int ||
+         ', '||
+         quote_literal(qry.matchfield) ||
+         ', '
+         || quote_literal(concat('zoning_2012_staging.', qry.shapefile_name)) ||
+         ') as codes(geom_id bigint,
+                    zoning_id varchar,
+                    area double precision,
+                    prop double precision,
+                    geom geometry);
+     '
+        AS sql_statement
+        FROM
+            (
+                select shapefile_name, substring(matchfield from 1 for 10) as matchfield, geoid10_int
+                    FROM zoning_staging.shapefile_metadata
+            ) qry
+        ) s;
+    IF exec THEN
+        EXECUTE sql_string;
+        RETURN 'Success!';
+    ELSE
+        RETURN sql_string;
+    END IF;
+END;
+$function$
+LANGUAGE plpgsql;
 
 
 
