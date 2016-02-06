be4e29784e0b9041fd07ba36b417cb4b5c642449
Tom Buckley <buckley.tom@gmail.com>
2016-02-05 17:31:50 -0800

    consolidate used functions in 1 script
    that way we can just load all of them at once and keep the directory listing shorter.

--- functions/get_zoning_id.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION zoning.get_id(name text,juris int)
-   RETURNS int AS
-$$
-  SELECT id 
-  from zoning.codes_dictionary 
-  WHERE name = $1 
-  AND juris = $2;
-$$
-  LANGUAGE sql;
