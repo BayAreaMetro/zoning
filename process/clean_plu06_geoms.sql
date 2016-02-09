86bf68db4f337165d7454ded77b6994c619b2ac7
Tom Buckley <buckley.tom@gmail.com>
2016-02-09 10:02:49 -0800

    update names in plu06 imports/cleaning

--- process/clean_plu06_geoms.sql
+++ process/clean_plu06_geoms.sql
@@ -1,31 +1,22 @@
-/*DROP TABLE zoning.plu06_may2015estimate_valid;
-CREATE TABLE zoning.plu06_may2015estimate_valid AS
-SELECT 
-	juris, zoning, ST_MakeValid(geom) geom
-FROM
-	zoning.plu06_may2015estimate;
-COMMENT ON TABLE zoning.plu06_may2015estimate_valid is 'subset of zoning.plu06_may2015estimate_source with valid geometries only';
-*/
-
-DROP TABLE zoning.plu06_may2015estimate_invalid;
-CREATE TABLE zoning.plu06_may2015estimate_invalid AS
+DROP TABLE zoning_staging.plu06_may2015estimate_invalid;
+CREATE TABLE zoning_staging.plu06_may2015estimate_invalid AS
 SELECT *
-FROM zoning.plu06_may2015estimate
+FROM zoning_staging.plu06_may2015estimate
 WHERE ST_IsValid(geom) = false;
-COMMENT ON TABLE zoning.plu06_may2015estimate_invalid is 'subset of zoning.plu06_may2015estimate_source with invalid geometries only';
+COMMENT ON TABLE zoning_staging.plu06_may2015estimate_invalid is 'subset of zoning_staging.plu06_may2015estimate_source with invalid geometries only';
 
-UPDATE zoning.plu06_may2015estimate
+UPDATE zoning_staging.plu06_may2015estimate
 	SET geom = ST_MakeValid(geom);
 
-DROP TABLE zoning.plu06_may2015estimate_geometry_collection;
-CREATE TABLE zoning.plu06_may2015estimate_geometry_collection AS 
+DROP TABLE zoning_staging.plu06_may2015estimate_geometry_collection;
+CREATE TABLE zoning_staging.plu06_may2015estimate_geometry_collection AS
 SELECT *
-FROM zoning.plu06_may2015estimate
+FROM zoning_staging.plu06_may2015estimate
 WHERE GeometryType(geom) <> 'MULTIPOLYGON';
-COMMENT ON TABLE zoning.geometry_collection is 'subset of zoning.plu06_may2015estimate with non multipolygon geometries produced by makevalid';
+COMMENT ON TABLE zoning_staging.geometry_collection is 'subset of zoning_staging.plu06_may2015estimate with non multipolygon geometries produced by makevalid';
 
-DELETE FROM zoning.plu06_may2015estimate
+DELETE FROM zoning_staging.plu06_may2015estimate
 WHERE GeometryType(geom) <> 'MULTIPOLYGON';
 
-ALTER TABLE zoning.plu06_may2015estimate
+ALTER TABLE zoning_staging.plu06_may2015estimate
  ALTER COLUMN geom TYPE geometry(MULTIPOLYGON, 26910);
--- process/overlaps_plu06_missing_parcels.sql
+++ process/overlaps_plu06_missing_parcels.sql
@@ -1,9 +1,9 @@
-CREATE INDEX ON zoning.unmapped_parcel_intersection_count using btree (geom_id);
-CREATE INDEX ON zoning.parcel_contested3 using btree (geom_id);
-vacuum (analyze) zoning.parcel_contested3;
-vacuum (analyze) zoning.unmapped_parcel_intersection_count;
-DROP TABLE IF EXISTS zoning.parcel_overlaps_plu;
-CREATE TABLE zoning.parcel_overlaps_plu AS
+CREATE INDEX ON zoning_staging.unmapped_parcel_intersection_count using btree (geom_id);
+CREATE INDEX ON zoning_staging.parcel_contested3 using btree (geom_id);
+vacuum (analyze) zoning_staging.parcel_contested3;
+vacuum (analyze) zoning_staging.unmapped_parcel_intersection_count;
+DROP TABLE IF EXISTS zoning_staging.parcel_overlaps_plu;
+CREATE TABLE zoning_staging.parcel_overlaps_plu AS
 SELECT 
 	geom_id,
 	plu06_objectid,
@@ -17,12 +17,12 @@ FROM (
 	 	ST_Intersection(p.geom, z.geom) geom 
 	FROM 
 		(select geom_id, geom 
-			FROM zoning.parcel_contested3
+			FROM zoning_staging.parcel_contested3
 			WHERE geom_id in 
 				(select geom_id 
-					from zoning.unmapped_parcel_intersection_count 
+					from zoning_staging.unmapped_parcel_intersection_count
 					WHERE countof>1)) as p,
-				(select objectid, geom from zoning.plu06_may2015estimate) as z
+				(select objectid, geom from zoning_staging.plu06_may2015estimate) as z
 		WHERE ST_Intersects(z.geom, p.geom)
 		) f
 GROUP BY 
